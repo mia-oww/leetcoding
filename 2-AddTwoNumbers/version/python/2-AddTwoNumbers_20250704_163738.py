ea4d18be32d18a526cf2ec2b3fe6f92a766258fc
Mia Gomez <miafendi01@gmail.com>
2025-07-04 16:37:36 -0500

    Create 2-AddTwoNumbers

---
+++ 2-AddTwoNumbers/version/python/2-AddTwoNumbers_20250704_163738.py
@@ -0,0 +1,9 @@
+# Last updated: 7/4/2025, 4:37:38 PM
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
+        
