614b475de9e1c2bddd7267b62f0d731ac3fb9774
Mia Gomez <miafendi01@gmail.com>
2025-07-24 00:31:06 -0500

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,20 @@
+# Last updated: 7/24/2025, 12:31:08 AM
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # two-pointer method
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum(): # skipping non-alphanumeric
+                left += 1 
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower(): # check lowercase
+                return False
+            left+= 1 
+            right -= 1
+
+        return True
+
+        
